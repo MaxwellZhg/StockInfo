87e53962a413f32b90151d4d31b9810d31789450
pengxianglin <pengxianglin@admin.com>
2019-08-23 13:50:33 +0800

    调用添加自选股接口

--- market/src/main/java/com/zhuorui/securities/market/ui/presenter/StockSearchPresenter.kt
+++ market/src/main/java/com/zhuorui/securities/market/ui/presenter/StockSearchPresenter.kt
@@ -1,15 +1,18 @@
 package com.zhuorui.securities.market.ui.presenter
 
 import com.zhuorui.securities.base2app.Cache
+import com.zhuorui.securities.base2app.network.BaseResponse
 import com.zhuorui.securities.base2app.network.Network
 import com.zhuorui.securities.base2app.rxbus.EventThread
 import com.zhuorui.securities.base2app.rxbus.RxBus
 import com.zhuorui.securities.base2app.rxbus.RxSubscribe
 import com.zhuorui.securities.base2app.ui.fragment.AbsNetPresenter
+import com.zhuorui.securities.infomation.config.LocalAccountConfig
 import com.zhuorui.securities.market.R
 import com.zhuorui.securities.market.event.AddTopicStockEvent
 import com.zhuorui.securities.market.model.SearchStockInfo
 import com.zhuorui.securities.market.net.IStockNet
+import com.zhuorui.securities.market.net.request.CollectionStockRequest
 import com.zhuorui.securities.market.net.request.StockSearchRequest
 import com.zhuorui.securities.market.net.response.StockSearchResponse
 import com.zhuorui.securities.market.ui.SearchStocksAdapter
@@ -51,8 +54,28 @@ class StockSearchPresenter : AbsNetPresenter<StockSearchView, StockSearchViewMod
 
     fun onAddTopicClickItem(item: SearchStockInfo?) {
         // 点击添加到自选列表
-        item?.let { AddTopicStockEvent(it) }?.let { RxBus.getDefault().post(it) }
-        toast(R.string.add_topic_successful)
+        if (LocalAccountConfig.read().isLogin()) {
+            // 已登录
+            val requset =
+                CollectionStockRequest(item!!, item.type!!, item.ts!!, item.code!!, 0, transactions.createTransaction())
+            Cache[IStockNet::class.java]?.collection(requset)
+                ?.enqueue(Network.IHCallBack<BaseResponse>(requset))
+        } else {
+            // 未登录
+            item?.let { AddTopicStockEvent(it) }?.let {
+                RxBus.getDefault().post(it)
+                toast(R.string.add_topic_successful)
+            }
+        }
+    }
+
+    override fun onBaseResponse(response: BaseResponse) {
+        super.onBaseResponse(response)
+        if (response.request is CollectionStockRequest) {
+            val stockInfo = (response.request as CollectionStockRequest).custom as SearchStockInfo
+            RxBus.getDefault().post(AddTopicStockEvent(stockInfo))
+            toast(R.string.add_topic_successful)
+        }
     }
 
     fun getAdapter(): SearchStocksAdapter? {
