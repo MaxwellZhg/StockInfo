7a99d787c9e5b739e6c1d475f2e55566a9dfd310
pengxianglin <pengxianglin@admin.com>
2019-08-29 15:34:56 +0800

    风险内容语音播放

--- openaccount/src/main/java/com/zhuorui/securities/openaccount/ui/viewmodel/OARiskDisclosureViewModel.kt
+++ openaccount/src/main/java/com/zhuorui/securities/openaccount/ui/viewmodel/OARiskDisclosureViewModel.kt
@@ -1,5 +1,6 @@
 package com.zhuorui.securities.openaccount.ui.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 /**
@@ -9,4 +10,13 @@ import androidx.lifecycle.ViewModel
  *    desc   :
  */
 class OARiskDisclosureViewModel : ViewModel() {
+
+    /**
+     * 风险披露播放状态
+     */
+    val playingRisk: MutableLiveData<Boolean> = MutableLiveData()
+
+    init {
+        playingRisk.value = false
+    }
 }
