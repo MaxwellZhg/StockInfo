2a82248d67e6a455ef5831946bfee9e27b5b59a1
pengxianglin <pengxianglin@admin.com>
2019-09-11 15:13:04 +0800

    新增本地纪录设置config

---
+++ personal/src/main/java/com/zhuorui/securities/personal/model/AppLanguage.kt
@@ -0,0 +1,15 @@
+package com.zhuorui.securities.personal.model
+
+/**
+ *    author : PengXianglin
+ *    e-mail : peng_xianglin@163.com
+ *    date   : 2019/9/11 15:01
+ *    desc   : 设置应用语言
+ */
+enum class AppLanguage {
+    auto,// 自动
+    zh_CN,//（简体中文）
+    zh_HK,//（繁体中文-香港）
+    zh_TW,//（繁体中文-台湾）
+    en_US//（英文）
+}
---
+++ personal/src/main/java/com/zhuorui/securities/personal/model/StocksThemeColor.kt
@@ -0,0 +1,11 @@
+package com.zhuorui.securities.personal.model
+
+/**
+ *    author : PengXianglin
+ *    e-mail : peng_xianglin@163.com
+ *    date   : 2019/9/11 14:55
+ *    desc   : 股票跌涨颜色
+ */
+enum class StocksThemeColor {
+    redUpGreenDown, greenUpRedDown
+}
