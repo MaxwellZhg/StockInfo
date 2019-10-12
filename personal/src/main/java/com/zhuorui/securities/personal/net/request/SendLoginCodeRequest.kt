fa979700a475d39c74d4e1bb45e44280fea6093b
zhaohuaguo <zhaohuaguo@admin.com>
2019-10-12 13:36:51 +0800

    修改request

---
+++ personal/src/main/java/com/zhuorui/securities/personal/net/request/SendLoginCodeRequest.kt
@@ -0,0 +1,21 @@
+package com.zhuorui.securities.personal.net.request
+
+import com.alibaba.fastjson.annotation.JSONField
+import com.zhuorui.securities.base2app.network.BaseRequest
+
+/**
+ * Created by Maxwell.
+ * E-mail: maxwell_smith@163.com
+ * Date: 2019/8/15
+ * Desc:
+ */
+class SendLoginCodeRequest(val phone: String, val countryCode: String, transaction: String) : BaseRequest(transaction) {
+
+
+    @JSONField(name = "isSend")
+    var isSend = false
+
+    init {
+        generateSign()
+    }
+}
