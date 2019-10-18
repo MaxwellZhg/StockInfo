fc834a2c0ad9e1d1fba64dbef5f9eaa44b609f7c
zhaohuaguo <zhaohuaguo@admin.com>
2019-10-18 19:22:56 +0800

    封装 组件

--- market/src/main/java/com/zhuorui/securities/market/ui/HkStockDetailFragment.kt
+++ market/src/main/java/com/zhuorui/securities/market/ui/HkStockDetailFragment.kt
@@ -1,5 +1,6 @@
 package com.zhuorui.securities.market.ui
 
+import android.os.Bundle
 import androidx.lifecycle.ViewModelProviders
 import com.zhuorui.securities.base2app.ui.fragment.AbsSwipeBackNetFragment
 import com.zhuorui.securities.market.BR
@@ -7,6 +8,7 @@ import com.zhuorui.securities.market.R
 import com.zhuorui.securities.market.ui.presenter.HkStockDetailPresenter
 import com.zhuorui.securities.market.ui.view.HkStockDetailView
 import com.zhuorui.securities.market.ui.viewmodel.HkStockDetailViewModel
+import kotlinx.android.synthetic.main.item_stock_detail_header.*
 
 /**
  * Created by Maxwell.
@@ -14,7 +16,9 @@ import com.zhuorui.securities.market.ui.viewmodel.HkStockDetailViewModel
  * Date: 2019/10/17
  * Desc:
  */
-class HkStockDetailFragment :AbsSwipeBackNetFragment<com.zhuorui.securities.market.databinding.FragmentHkStockDetailBinding,HkStockDetailViewModel,HkStockDetailView,HkStockDetailPresenter>(),HkStockDetailView{
+class HkStockDetailFragment :
+    AbsSwipeBackNetFragment<com.zhuorui.securities.market.databinding.FragmentHkStockDetailBinding, HkStockDetailViewModel, HkStockDetailView, HkStockDetailPresenter>(),
+    HkStockDetailView {
     override val layout: Int
         get() = R.layout.fragment_hk_stock_detail
     override val viewModelId: Int
@@ -25,5 +29,27 @@ class HkStockDetailFragment :AbsSwipeBackNetFragment<com.zhuorui.securities.mark
         get() = ViewModelProviders.of(this).get(HkStockDetailViewModel::class.java)
     override val getView: HkStockDetailView
         get() = this
+
+    companion object {
+        fun newInstance(): HkStockDetailFragment {
+            return HkStockDetailFragment()
+        }
+    }
+
+    override fun onLazyInitView(savedInstanceState: Bundle?) {
+        super.onLazyInitView(savedInstanceState)
+        zr_line.setType(1)
+        zr_line.setValues(40,10,50)
+        zr_line_text.setType(0)
+        zr_line_text.setValues(40,10,50)
+        zr_line1.setType(1)
+        zr_line1.setValues(50,40,30)
+        zr_line_text1.setType(0)
+        zr_line_text1.setValues(50,40,30)
+        zr_line2.setType(1)
+        zr_line2.setValues(20,40,50)
+        zr_line_text2.setType(0)
+        zr_line_text2.setValues(20,40,50)
+    }
 
 }
