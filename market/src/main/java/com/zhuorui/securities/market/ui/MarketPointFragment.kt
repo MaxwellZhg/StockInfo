496628d12cb19c81ae9be058e04626ca67e6edb1
zhaohuaguo <zhaohuaguo@admin.com>
2019-10-23 16:36:32 +0800

    搭建指数模块UI

--- market/src/main/java/com/zhuorui/securities/market/ui/MarketPointFragment.kt
+++ market/src/main/java/com/zhuorui/securities/market/ui/MarketPointFragment.kt
@@ -1,9 +1,11 @@
 package com.zhuorui.securities.market.ui
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.ViewModelProviders
 import com.zhuorui.securities.base2app.ui.fragment.AbsSwipeBackNetFragment
+import com.zhuorui.securities.base2app.util.ResUtil
 import com.zhuorui.securities.market.BR
 import com.zhuorui.securities.market.R
 import com.zhuorui.securities.market.ui.presenter.MarketPointPresenter
@@ -11,7 +13,18 @@ import com.zhuorui.securities.market.ui.view.MarketPointView
 import com.zhuorui.securities.market.ui.viewmodel.MarketPointViewModel
 import com.zhuorui.securities.market.databinding.FragmentMarketPointBinding
 import com.zhuorui.securities.market.generated.callback.OnClickListener
+import com.zhuorui.securities.market.ui.adapter.MarketPartInfoAdapter
+import com.zhuorui.securities.market.ui.kline.KlineFragment
+import kotlinx.android.synthetic.main.fragment_hk_stock_detail.*
+import kotlinx.android.synthetic.main.fragment_market_point.*
 import kotlinx.android.synthetic.main.layout_market_point_topbar.*
+import net.lucode.hackware.magicindicator.abs.IPagerNavigator
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.CommonNavigator
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.CommonNavigatorAdapter
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerIndicator
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerTitleView
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.indicators.LinePagerIndicator
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.ColorTransitionPagerTitleView
 
 /**
  * Created by Maxwell.
@@ -20,6 +33,8 @@ import kotlinx.android.synthetic.main.layout_market_point_topbar.*
  * Desc:
  */
 class MarketPointFragment :AbsSwipeBackNetFragment<FragmentMarketPointBinding,MarketPointViewModel,MarketPointView,MarketPointPresenter>(),MarketPointView,View.OnClickListener{
+    private var infoadapter: MarketPartInfoAdapter? = null
+    private var tabTitle:ArrayList<String> = ArrayList()
     override val layout: Int
         get() = R.layout.fragment_market_point
     override val viewModelId: Int
@@ -42,8 +57,21 @@ class MarketPointFragment :AbsSwipeBackNetFragment<FragmentMarketPointBinding,Ma
 
     override fun onLazyInitView(savedInstanceState: Bundle?) {
         super.onLazyInitView(savedInstanceState)
+        tabTitle.add("成分股")
+        tabTitle.add("咨讯")
         iv_back.setOnClickListener(this)
         iv_search.setOnClickListener(this)
+        loadRootFragment(R.id.kline_view, KlineFragment())
+        magic_indicator.navigator=getNavigator()
+        presenter?.setLifecycleOwner(this)
+        infoadapter = presenter?.getMarketInfoAdapter()
+        //解决数据加载不完的问题
+        rv_point_stock.isNestedScrollingEnabled = false
+        rv_point_stock.setHasFixedSize(true)
+        rv_point_stock.isFocusable = false
+        presenter?.getData()
+        infoadapter?.notifyDataSetChanged()
+        rv_point_stock.adapter=infoadapter
     }
     override fun onClick(v: View?) {
         when(v){
@@ -56,5 +84,59 @@ class MarketPointFragment :AbsSwipeBackNetFragment<FragmentMarketPointBinding,Ma
         }
 
     }
+
+    /**
+     * 获取指示器适配器
+     */
+    private fun getNavigator(): IPagerNavigator? {
+        // 设置viewpager指示器
+        val commonNavigator = CommonNavigator(requireContext())
+        commonNavigator.isAdjustMode = false
+        commonNavigator.adapter = object : CommonNavigatorAdapter() {
+
+            override fun getCount(): Int {
+                return tabTitle?.size!!
+            }
+
+            override fun getTitleView(context: Context, index: Int): IPagerTitleView {
+                val colorTransitionPagerTitleView = ColorTransitionPagerTitleView(context)
+                colorTransitionPagerTitleView.normalColor =
+                    ResUtil.getColor(R.color.color_B3BCD0)!!
+                colorTransitionPagerTitleView.selectedColor =
+                    ResUtil.getColor(R.color.tab_select)!!
+                colorTransitionPagerTitleView.textSize = 18f
+                colorTransitionPagerTitleView.text = tabTitle!![index]
+                colorTransitionPagerTitleView.setOnClickListener {
+                    magic_indicator.onPageSelected(index)
+                    magic_indicator.onPageScrolled(index, 0.0F, 0)
+                    onSelect(index)
+                }
+                return colorTransitionPagerTitleView
+            }
+
+            override fun getIndicator(context: Context): IPagerIndicator {
+                val indicator = LinePagerIndicator(context)
+                indicator.mode = LinePagerIndicator.MODE_WRAP_CONTENT
+                indicator.setColors(ResUtil.getColor(R.color.tab_select))
+                indicator.lineHeight = ResUtil.getDimensionDp2Px(2f).toFloat()
+                indicator.lineWidth = ResUtil.getDimensionDp2Px(33f).toFloat()
+                return indicator
+            }
+        }
+        return commonNavigator
+    }
+
+    private fun onSelect(index: Int) {
+
+    }
+
+    override fun addInfoToAdapter(list: List<Int>) {
+        infoadapter?.clearItems()
+        if (infoadapter?.items == null) {
+            infoadapter?.items = ArrayList()
+        }
+        infoadapter?.addItems(list)
+    }
+
 
 }
