ddcc3d4028ddfe035d7346b712d357729a38841d
pengxianglin <pengxianglin@admin.com>
2019-11-06 13:40:59 +0800

    创建F10页面

---
+++ market/src/main/java/com/zhuorui/securities/market/ui/MarketDetailF10BriefFragment.kt
@@ -0,0 +1,16 @@
+package com.zhuorui.securities.market.ui
+
+/**
+ *    author : PengXianglin
+ *    e-mail : peng_xianglin@163.com
+ *    date   : 2019/11/6 13:36
+ *    desc   : F10简况页面
+ */
+class MarketDetailF10BriefFragment {
+
+    companion object {
+        fun newInstance(): MarketDetailF10BriefFragment {
+            return MarketDetailF10BriefFragment()
+        }
+    }
+}
