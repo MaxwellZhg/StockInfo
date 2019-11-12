cfc06c817ba89d5167a90c9af5119dac44010a31
pengxianglin <pengxianglin@admin.com>
2019-11-12 15:51:04 +0800

    update

--- market/src/main/java/com/zhuorui/securities/market/ui/MarketDetailF10FinancialFragment.kt
+++ market/src/main/java/com/zhuorui/securities/market/ui/MarketDetailF10FinancialFragment.kt
@@ -2,9 +2,10 @@ package com.zhuorui.securities.market.ui
 
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProviders
-import com.zhuorui.securities.base2app.ui.fragment.AbsSwipeBackNetFragment
+import com.zhuorui.securities.base2app.ui.fragment.AbsFragment
 import com.zhuorui.securities.market.BR
 import com.zhuorui.securities.market.R
+import com.zhuorui.securities.market.databinding.FragmentMarketDetailF10FinancialBinding
 import com.zhuorui.securities.market.ui.presenter.MarketDetailF10FinancialPresenter
 import com.zhuorui.securities.market.ui.view.MarketDetailF10FinancialView
 import com.zhuorui.securities.market.ui.viewmodel.MarketDetailF10FinancialViewModel
@@ -16,20 +17,29 @@ import kotlinx.android.synthetic.main.fragment_market_detail_f10_financial.*
  *    date   : 2019/11/6 13:35
  *    desc   : F10财报页面
  */
-class MarketDetailF10FinancialFragment : AbsSwipeBackNetFragment<com.zhuorui.securities.market.databinding.FragmentMarketDetailF10FinancialBinding,MarketDetailF10FinancialViewModel,MarketDetailF10FinancialView,MarketDetailF10FinancialPresenter>(),MarketDetailF10FinancialView {
+class MarketDetailF10FinancialFragment :
+    AbsFragment<FragmentMarketDetailF10FinancialBinding, MarketDetailF10FinancialViewModel, MarketDetailF10FinancialView, MarketDetailF10FinancialPresenter>(),
+    MarketDetailF10FinancialView {
+
     override val layout: Int
         get() = R.layout.fragment_market_detail_f10_financial
+
     override val viewModelId: Int
         get() = BR.viewModel
+
     override val createPresenter: MarketDetailF10FinancialPresenter
         get() = MarketDetailF10FinancialPresenter()
+
     override val createViewModel: MarketDetailF10FinancialViewModel?
         get() = ViewModelProviders.of(this).get(MarketDetailF10FinancialViewModel::class.java)
+
     override val getView: MarketDetailF10FinancialView
         get() = this
+
     private var stockCode: String? = null
+
     companion object {
-        fun newInstance(stockCode:String): MarketDetailF10FinancialFragment {
+        fun newInstance(stockCode: String): MarketDetailF10FinancialFragment {
             val fragment = MarketDetailF10FinancialFragment()
             if (stockCode != null) {
                 val bundle = Bundle()
