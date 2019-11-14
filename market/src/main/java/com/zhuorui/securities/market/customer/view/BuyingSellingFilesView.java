2236b8c41d77c9ee6cd449661019881e1efa60c3
刘伟 <1144390791@qq.com>
2019-11-14 17:19:08 +0800

    个股行情 买卖十档ui

--- market/src/main/java/com/zhuorui/securities/market/customer/view/BuyingSellingFilesView.java
+++ market/src/main/java/com/zhuorui/securities/market/customer/view/BuyingSellingFilesView.java
@@ -1,8 +1,6 @@
 package com.zhuorui.securities.market.customer.view;
 
-import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
-import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Color;
 import android.util.AttributeSet;
@@ -13,9 +11,14 @@ import android.widget.FrameLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import com.zhuorui.commonwidget.config.LocalSettingsConfig;
+import com.zhuorui.securities.base2app.util.ResUtil;
 import com.zhuorui.securities.market.R;
+import com.zhuorui.securities.market.util.MarketUtil;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
@@ -33,7 +36,9 @@ public class BuyingSellingFilesView extends FrameLayout {
     private ProgressBar vPB;
     private TextView vBuyingValue;
     private TextView vSellingValue;
-    private MyAdapter mAdapter;
+    private BuySellFileAdapter mAdapter;
+    private final int defColor;
+    private final LocalSettingsConfig config;
 
     public BuyingSellingFilesView(Context context) {
         this(context, null);
@@ -45,8 +50,26 @@ public class BuyingSellingFilesView extends FrameLayout {
 
     public BuyingSellingFilesView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        inflate(context, R.layout.view_buying_selling_files, this);
-        initView();
+        defColor = Color.parseColor("C0CCE0");
+        config = LocalSettingsConfig.Companion.getInstance();
+        int type = 0;
+        if (type == 1) {
+            inflate(context, R.layout.view_buying_selling_files_linear, this);
+            initView();
+            vRv.setLayoutManager(new LinearLayoutManager(getContext()));
+            vRv.setAdapter(mAdapter = new LinearAdapter(getContext()));
+            post(() -> {
+                int itemHight = mAdapter.calculationItemHight(getHeight());
+                ConstraintLayout.LayoutParams lp = (ConstraintLayout.LayoutParams) vPB.getLayoutParams();
+                lp.height = itemHight;
+                vPB.requestLayout();
+            });
+        } else {
+            inflate(context, R.layout.view_buying_selling_files_grid, this);
+            initView();
+            vRv.setLayoutManager(new GridLayoutManager(getContext(), 2));
+            vRv.setAdapter(mAdapter = new GridAdapter(getContext()));
+        }
     }
 
     private void initView() {
@@ -54,42 +77,46 @@ public class BuyingSellingFilesView extends FrameLayout {
         vBuyingValue = findViewById(R.id.tv_progress1_value);
         vSellingValue = findViewById(R.id.tv_progress2_value);
         vRv = findViewById(R.id.recycler_view);
-        //解决数据加载不完的问题
         vRv.setNestedScrollingEnabled(false);
         vRv.setHasFixedSize(true);
-        //解决数据加载完成后, 没有停留在顶部的问题
         vRv.setFocusable(false);
-        RecyclerView.LayoutManager lm = new GridLayoutManager(getContext(), 2);
-        vRv.setLayoutManager(lm);
-        vRv.setAdapter(mAdapter = new MyAdapter(getContext()));
     }
 
-    public void setData(float buyingValue, float sellingValue, List<? extends Object> buyingData, List<? extends Object> sellingData) {
+    /**
+     * @param buyingValue
+     * @param sellingValue
+     * @param buyingData
+     * @param sellingData
+     */
+    public void setData(float buyingValue, float sellingValue, List<Object> buyingData, List<Object> sellingData) {
         float total = buyingValue + sellingValue;
         float buyingB = buyingValue / total;
         float sellingB = 1 - buyingB;
-        vPB.setProgress((int) (buyingB * 10000));
-        DecimalFormat decimalFormat = new DecimalFormat("0.00%");
-        vBuyingValue.setText(decimalFormat.format(buyingB));
-        vSellingValue.setText(decimalFormat.format(sellingB));
-        List<Object> datas = new ArrayList<>();
-        for (int i = 0, l = buyingData.size(); i < l; i++) {
-            datas.add(buyingData.get(i));
-            datas.add(sellingData.get(i));
-        }
-        mAdapter.setData(datas);
+        vPB.setProgress((int) (buyingB * 1000));
+        vBuyingValue.setText(String.format(ResUtil.INSTANCE.getString(R.string.buying_percentage), buyingB * 100));
+        vSellingValue.setText(String.format(ResUtil.INSTANCE.getString(R.string.selling_percentage), sellingB * 100));
+        mAdapter.setData(buyingData, sellingData);
         mAdapter.notifyDataSetChanged();
     }
 
-    class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
+    class GridAdapter extends BuySellFileAdapter {
 
         private Context context;
-        private List<Object> mDatas;
-        private String[] mTitles = {"买一", "卖一", "买二", "卖二", "买三", "卖三", "买四", "卖四", "买五", "卖五", "买六", "卖六", "买七", "卖七", "买八", "卖八", "买九", "卖九", "买十", "卖十"};
+        private final List<Object> mDatas;
+        private final int bay1color;
+        private final int sell1color;
+        private final int bayColor;
+        private final int sellColor;
+        private final String[] mTitles;
 
-        public MyAdapter(Context context) {
+        public GridAdapter(Context context) {
             this.context = context;
             mDatas = new ArrayList<>();
+            mTitles = context.getResources().getStringArray(R.array.buy_sell_file_grid);
+            bay1color = Color.parseColor("#17355E");
+            sell1color = Color.parseColor("#5D3A1C");
+            bayColor = Color.parseColor("#0D1A6ED2");
+            sellColor = Color.parseColor("#0DFF8E1B");
         }
 
         @NonNull
@@ -101,65 +128,134 @@ public class BuyingSellingFilesView extends FrameLayout {
 
         @Override
         public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-            holder.bindData(position, mTitles[position]);
+            int backgroundColor;
+            if (position == 0) {
+                backgroundColor = bay1color;
+            } else if (position == 1) {
+                backgroundColor = sell1color;
+            } else if (position % 2 == 0) {
+                backgroundColor = bayColor;
+            } else {
+                backgroundColor = sellColor;
+            }
+            holder.bindData(position, mTitles[position], mDatas.get(position), backgroundColor);
         }
 
         @Override
         public int getItemCount() {
-            int size = mDatas.size();
-            return size > 20 ? 20 : size;
+            return mDatas.size() > mTitles.length ? mTitles.length : mDatas.size();
         }
 
-        public void setData(List<Object> datas) {
+
+        @Override
+        public void setData(List<Object> buyingData, List<Object> sellingData) {
+            int size = Math.min(buyingData.size(), sellingData.size());
+            List<Object> datas = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                datas.add(buyingData.get(i));
+                datas.add(sellingData.get(i));
+            }
+            mDatas.clear();
+            mDatas.addAll(datas);
+        }
+
+    }
+
+    class LinearAdapter extends BuySellFileAdapter {
+
+        private Context context;
+        private final List<Object> mDatas;
+        private final String[] mTitles;
+        private int mItemHight;
+
+        public LinearAdapter(Context context) {
+            this.context = context;
+            mDatas = new ArrayList<>();
+            mTitles = context.getResources().getStringArray(R.array.buy_sell_file_linear);
+        }
+
+        @Override
+        public int calculationItemHight(int totalHight) {
+            mItemHight = totalHight / mTitles.length;
+            return mItemHight;
+        }
+
+        @NonNull
+        @Override
+        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(context).inflate(R.layout.item_buying_selling_files, parent, false);
+            View rootView = v.findViewById(R.id.root_view);
+            RecyclerView.LayoutParams lp = new RecyclerView.LayoutParams(RecyclerView.LayoutParams.MATCH_PARENT, mItemHight);
+            rootView.setLayoutParams(lp);
+            return new MyViewHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+            holder.bindData(position, mTitles[position], mDatas.get(position), 0);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mDatas.size() > mTitles.length ? mTitles.length : mDatas.size();
+        }
+
+
+        @Override
+        public void setData(List<Object> buyingData, List<Object> sellingData) {
+            int size = (mTitles.length - 1) / 2;
+            size = Math.min(size, Math.min(buyingData.size(), sellingData.size()));
+            List<Object> datas = new ArrayList<>();
+            for (int i = size - 1; i >= 0; i--) {
+                datas.add(buyingData.get(i));
+            }
+            for (int i = 0; i < size; i++) {
+                datas.add(sellingData.get(i));
+            }
             mDatas.clear();
             mDatas.addAll(datas);
         }
     }
 
+    private abstract class BuySellFileAdapter extends RecyclerView.Adapter<MyViewHolder> {
+
+        public abstract void setData(List<Object> buyingData, List<Object> sellingData);
+
+        public int calculationItemHight(int totalHight) {
+            return 0;
+        }
+
+    }
+
     class MyViewHolder extends RecyclerView.ViewHolder {
-        private final int color1;
-        private final int color2;
-        private final int color3;
-        private final int color4;
+
         private TextView vTitle;
         private TextView vPirce;
         private TextView vNum;
+        private TextView vAnim;
+
+        private ObjectAnimator animator;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
-            color1 = Color.parseColor("#17355E");
-            color2 = Color.parseColor("#5D3A1C");
-            color3 = Color.parseColor("#0D1A6ED2");
-            color4 = Color.parseColor("#0DFF8E1B");
             vTitle = itemView.findViewById(R.id.tv_title);
             vPirce = itemView.findViewById(R.id.tv_price);
             vNum = itemView.findViewById(R.id.tv_num);
+            vAnim = itemView.findViewById(R.id.v_anim);
+            itemView.setOnClickListener(v -> animator = MarketUtil.showUpDownAnim(animator, vAnim, true));
         }
 
-        public void bindData(int position, String title) {
+        public void bindData(int position, String title, Object data, int backgroundColor) {
             vTitle.setText(title);
-            vPirce.setText(String.format("%.3f", 90.253 + position));
-            vNum.setText(String.format("%.1fK(%2d)", position + 1.2,position));
-            if (position == 0) {
-                itemView.setBackgroundColor(color1);
-            } else if (position == 1) {
-                itemView.setBackgroundColor(color2);
-            } else {
-                int i = position % 2;
-                if (i == 0) {
-                    itemView.setBackgroundColor(color3);
-                } else {
-                    itemView.setBackgroundColor(color4);
-                }
-            }
-            itemView.setOnClickListener(v->{
-                ValueAnimator colorAnim = ObjectAnimator.ofInt(vNum,"backgroundColor", 0x00000000,0x33D9001B, 0x00000000);
-                colorAnim.setDuration(400);
-                colorAnim.setEvaluator(new ArgbEvaluator());
-                colorAnim.setRepeatMode(ValueAnimator.REVERSE);
-                colorAnim.start();
-            });
+            float price = 90.253f + position;
+            float preClosePrice = 0;
+            vPirce.setTextColor(config.getUpDownColor(price, preClosePrice, defColor));
+            vPirce.setText(String.format("%.3f", price));
+            vNum.setText(String.format("%.1fK(%2d)", position + 1.2, position));
+            itemView.setBackgroundColor(backgroundColor);
         }
+
+
     }
 
 }
