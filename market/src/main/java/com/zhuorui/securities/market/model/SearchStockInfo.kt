d63f22f0876afd3387c89d492c95ebe2ea061365
pengxianglin <pengxianglin@admin.com>
2019-11-28 19:05:02 +0800

    修改搜索自选股字段

--- market/src/main/java/com/zhuorui/securities/market/model/SearchStockInfo.kt
+++ market/src/main/java/com/zhuorui/securities/market/model/SearchStockInfo.kt
@@ -3,6 +3,7 @@ package com.zhuorui.securities.market.model
 import android.os.Parcelable
 import kotlinx.android.parcel.IgnoredOnParcel
 import kotlinx.android.parcel.Parcelize
+import java.math.BigDecimal
 
 /**
  *    author : PengXianglin
@@ -14,10 +15,25 @@ import kotlinx.android.parcel.Parcelize
 @Parcelize
 open class SearchStockInfo : BaseStockMarket(), Parcelable {
 
+    // 股票代码 600004.SH
     @IgnoredOnParcel
-    var tsCode: String? = null//股票代码 600004.SH
+    var tsCode: String? = null
+    // 名称
     @IgnoredOnParcel
-    var name: String? = null//名称
+    var name: String? = null
+    // 是否收藏
     @IgnoredOnParcel
-    var collect:Boolean =false//是否收藏
+    var collect: Boolean = false
+    // 当前价格：如13.75
+    @IgnoredOnParcel
+    var last: BigDecimal? = null
+    // 昨收
+    @IgnoredOnParcel
+    var preClose: BigDecimal? = null
+    // 涨跌幅：如-0.0018（-0.18%）
+    @IgnoredOnParcel
+    var diffRate: BigDecimal? = null
+    // 股市状态
+    @IgnoredOnParcel
+    var suspension: StockSuspension? = StockSuspension.empty
 }
