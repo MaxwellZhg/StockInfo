4b3ec4b5ce36b6eca16e78cc47f77779aed75764
pengxianglin <pengxianglin@admin.com>
2019-11-29 16:50:30 +0800

    格式化

--- personal/src/main/java/com/zhuorui/securities/personal/ui/SettingFragment.kt
+++ personal/src/main/java/com/zhuorui/securities/personal/ui/SettingFragment.kt
@@ -1,27 +1,17 @@
 package com.zhuorui.securities.personal.ui
 
 import android.os.Bundle
-import android.view.View
 import androidx.lifecycle.ViewModelProviders
-import com.zhuorui.commonwidget.common.CommonEnum
-import com.zhuorui.securities.base2app.adapter.BaseListAdapter
-import com.zhuorui.securities.base2app.rxbus.RxBus
-import com.zhuorui.securities.base2app.ui.fragment.AbsSwipeBackFragment
 import com.zhuorui.securities.base2app.ui.fragment.AbsSwipeBackNetFragment
 import com.zhuorui.securities.base2app.util.ResUtil
 import com.zhuorui.securities.personal.BR
 import com.zhuorui.securities.personal.R
 import com.zhuorui.securities.personal.databinding.FragmentSettingBinding
-import com.zhuorui.securities.personal.event.SettingChooseEvent
-import com.zhuorui.securities.personal.ui.adapter.HelpCenterInfoAdapter
 import com.zhuorui.securities.personal.ui.adapter.SettingDataAdapter
-import com.zhuorui.securities.personal.ui.model.SettingData
 import com.zhuorui.securities.personal.ui.presenter.SettingPresenter
 import com.zhuorui.securities.personal.ui.view.SettingView
 import com.zhuorui.securities.personal.ui.viewmodel.SettingViewModel
 import kotlinx.android.synthetic.main.fragment_setting.*
-import me.jessyan.autosize.utils.LogUtils
-import me.yokeyword.fragmentation.ISupportFragment
 
 /**
  * Created by Maxwell.
@@ -31,22 +21,31 @@ import me.yokeyword.fragmentation.ISupportFragment
  */
 class SettingFragment : AbsSwipeBackNetFragment<FragmentSettingBinding, SettingViewModel, SettingView, SettingPresenter>(),
     SettingView{
+
     private var type: Int = 0
+
     private var tips : String? =null
+
     private var adapter: SettingDataAdapter? = null
+
     override val layout: Int
         get() = R.layout.fragment_setting
+
     override val viewModelId: Int
         get() = BR.viewModel
+
     override val createPresenter: SettingPresenter
         get() = SettingPresenter()
+
     override val createViewModel: SettingViewModel?
         get() = ViewModelProviders.of(this).get(SettingViewModel::class.java)
+
     override val getView: SettingView
         get() = this
 
     override fun onLazyInitView(savedInstanceState: Bundle?) {
         super.onLazyInitView(savedInstanceState)
+
         type = arguments?.getSerializable("type") as Int
         tips = arguments?.getSerializable("tips") as String
         when (type) {
